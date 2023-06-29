31baaafae7843feecaab22b61b3106a0ff818910
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-29 20:30:11 +0200

    Reference Types And Primitive Types

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
-import java.sql.SQLOutput;
-import java.util.Date;
+import java.awt.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
-
-
-
-
-
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
