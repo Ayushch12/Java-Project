9979962b9f3778249ff143b1beaec57ae71c16b2
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-29 20:01:24 +0200

    Primitive Types

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-     int myAge = 388;
-     int herAge = myAge;
-        System.out.println(herAge);
+        byte age = 30;
+        long viewsCount = 3_123_456_789L ;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+
+
+
 
 
     }
