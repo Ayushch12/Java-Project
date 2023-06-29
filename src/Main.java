b5f5cf9b8c53cfca0d06566e0a616e42fb7b6c08
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-29 22:03:59 +0200

    Constants

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import java.sql.SQLOutput;
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
-        int [][] numbers = { {1,2,3},{4,5,6} };
-        numbers [0] [0] = 1;
-        System.out.println(Arrays.deepToString(numbers));
+      final float  PI = 3.14F;
+
 
     }
 }
