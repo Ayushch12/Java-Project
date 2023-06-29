56ebfa42ff30cdff20e1347ec783bb8fddfb0a6a
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-29 20:10:33 +0200

    Reference Types

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
+import java.sql.SQLOutput;
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         byte age = 30;
-        long viewsCount = 3_123_456_789L ;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-
+        Date now = new Date();
+        System.out.println(now);
 
 
 
