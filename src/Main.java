6b4e227fddde65543f96891cf02ce2c5d8878766
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-01 15:12:26 +0200

    Multi-Dimensional Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-      final float  PI = 3.14F;
+    int [] [] numbers = {{1, 2, 3},{4 ,5, 6}};
+        System.out.println(Arrays.deepToString(numbers));
 
 
     }
