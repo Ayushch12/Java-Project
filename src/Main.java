ffe384c285ba74a09335936cf498b83a19d1a829
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-01 15:33:21 +0200

    Casting

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-int X = (10 +3) * 2;
+ double x = 1.1 ;
+ double y =x+2;
 
-        System.out.println(X);
+        System.out.println(y);
     }
 }
-// Example 1: int X = 1;
-//   ++X ;
- // Order of Operation
-// (), */ , + -
