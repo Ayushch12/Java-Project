a4b9f5d46df1e7193e58fd7d14e134c1a6c6f790
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-01 15:25:35 +0200

    order of Operations

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-int X = 1;
-  X /= 2;
-        System.out.println(X);
+int X = (10 +3) * 2;
 
+        System.out.println(X);
     }
 }
 // Example 1: int X = 1;
