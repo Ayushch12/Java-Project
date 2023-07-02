8ff08e2e23cd1e6cebd9f43bbcdf795df956581f
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-02 14:51:14 +0200

    The Math Class-With int result = (int) (Math.random() * 100);

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
- int result = (int)Math.ceil(1.1F);
+ int result = (int) (Math.random() * 100);
 
         System.out.println(result);
     }
 }
+
+//For example we can use like max,ceil,float,
+// double result = Math.random();
+//
+
