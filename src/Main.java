f84cb6424f4091778060f9d5deb30813ca118356
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-02 15:21:02 +0200

    Formatting Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import java.text.NumberFormat;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
- int result = (int) (Math.random() * 100);
-
+        double number = 0.1;
+        String result = NumberFormat.getPercentInstance().format(number);
         System.out.println(result);
     }
 }
+
 
 //For example we can use like max,ceil,float,
 // double result = Math.random();
